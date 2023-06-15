b717baf110a3d5695c60d2582685c9fcb6a05e75
Jeremy Heckt <jeremy@jeremyheckt.com>
2023-06-15 14:58:52 -0700

    can fetch data, need to template HTML

--- garden_planner.py
+++ garden_planner.py
@@ -1,4 +1,33 @@
-import garden_schema.py as gs
+import garden_schema as gs # garden_schema.py, actually calls GPT-4 API
+import json
 
 
-print(gs.get_plant_data("tomato", "8b"))
+#####
+def get_test_schema():
+    """
+    Just generates some test data and saves it as a JSON so we can test the templating 
+    without repeated calls to OpenAI.
+
+    Output: test_data.json
+    """
+
+    plants = ["radishes", "potatoes", "turnips", "thyme"]
+    garden_object = {}
+
+    for plant in plants:
+        plant_data = gs.get_plant_data(plant, "8b")
+        garden_object[plant] = plant_data
+
+    with open("test_data.json", "w") as f:
+        jdata = json.dumps(garden_object)
+
+        f.write(jdata)
+#####
+
+if __name__ == "__main__":
+
+    with open("test_data.json", "r") as f:
+        data = json.loads(f.read())
+
+        for d in data:
+            print(data[d])
