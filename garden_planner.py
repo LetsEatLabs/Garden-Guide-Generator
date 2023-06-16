7437867cd47954802e6b2f27a6bceafd1cc45ee0
Jeremy Heckt <jeremy@jeremyheckt.com>
2023-06-15 19:47:58 -0700

    can now read the test data and make a PDF

--- garden_planner.py
+++ garden_planner.py
@@ -1,5 +1,10 @@
 import garden_schema as gs # garden_schema.py, actually calls GPT-4 API
-import json
+import json, os
+
+from pathlib import Path
+
+from weasyprint import HTML
+from jinja2 import Environment, FileSystemLoader
 
 
 #####
@@ -23,11 +28,39 @@ def get_test_schema():
 
         f.write(jdata)
 #####
+def sanitize_json(data):
+    """
+    Goes through the JSON and makes sure that all the values are valid JSON.
+    """
+
+    for key, value in data.items():
+            try:
+                data[key] = json.loads(value)
+            except (TypeError, json.JSONDecodeError):
+                # If it's a non-string value or not a valid JSON string, continue
+                pass
+                
+    return data
+
+#####
 
 if __name__ == "__main__":
+
+    root = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(root, 'templates')
+    env = Environment( loader = FileSystemLoader(templates_dir) )
+    template = env.get_template('template.html')
 
     with open("test_data.json", "r") as f:
         data = json.loads(f.read())
 
-        for d in data:
-            print(data[d])
+        data = sanitize_json(data)
+
+        filename = os.path.join(root, 'output.html')
+        with open(filename, 'w') as fh:
+            fh.write(template.render(plants=data))
+
+        htmldoc = HTML(string=open("output.html", "r").read(), base_url="")
+        
+
+        Path("out.pdf").write_bytes(htmldoc.write_pdf())
